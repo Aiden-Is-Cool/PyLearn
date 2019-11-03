eeb11f8bfe084e3241b129b32166d62a8d14ce8a
Aiden-Is-Cool <57082783+Aiden-Is_Cool@users.noreply.github.com>
2019-11-03 15:09:41 -0500

    Added Debugging Config File + Changed argument parser logic

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 import argparse
 parser = argparse.ArgumentParser()
 
-parser.add_argument("x", help="The X value.", type=int)
-parser.add_argument("y",  help="The Y value.", type=int)
-parser.add_argument("-v", "--verbosity", action="store_true", help="Verbose output")
+parser.add_argument("-x", help="The X value.", type=int)
+parser.add_argument("-y", help="The Y value.", type=int)
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
 
 args=parser.parse_args()
-x= args.x
-y=args.y
-answer = x+y
+x = args.x
+y = args.y
+answer = x*y
+
+print("=============================")
 print(answer)
-print(args.x)
-print(args.y)
 
 if args.verbose:
     print("Adding {} plus {} equals {}".format(args.x, args.y, answer))
